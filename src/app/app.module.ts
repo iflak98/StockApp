b6b53201fff5319b99dd36725dd7746202dfb62e
Iflak Yousuf Mir <iflak98@gmail.com>
2021-06-11 17:52:30 +0530

    stock complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { StockComponent} from './../StockDemo/stock.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule
